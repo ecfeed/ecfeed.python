749d7180edcdbe5e52729045584ca0f64c492e92
Patryk Chamuczyński <p.chamuczynski@radytek.com>
2020-04-05 22:14:32 +0200

    reworked ecfeed_cli to use TestProvider class

--- ecfeed_cli.py
+++ ecfeed_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from ecfeed import EcFeed, DataSource, TemplateType
+from ecfeed import TestProvider, DataSource, TemplateType
 import sys
 import os
 import json
@@ -7,7 +7,7 @@ import json
 def main():
     args = parse_arguments()
 
-    ecfeed = EcFeed(genserver=args['genserver'], keystore_path=args['keystore'], password=args['password'], model=args['model'])    
+    ecfeed = TestProvider(genserver=args['genserver'], keystore_path=args['keystore'], password=args['password'], model=args['model'])    
     if args['output'] != None:
         sys.stdout = open(args['output'], 'w')
 
@@ -41,25 +41,6 @@ def parse_arguments():
     connection_args.add_argument('--password', dest='password', action='store', help='Password to keystore. Default is "changeit"', default='changeit')
     connection_args.add_argument('--genserver', dest='genserver', action='store', help='Address of the ecfeed service. Default is "develop-gen.ecfeed.com"', default='develop-gen.ecfeed.com')
 
-    # gen_parsers = parser.add_subparsers(title='generators', description='data generator options', help=True, required=True)
-    # nwise_parser = gen_parsers.add_parser('--nwise')
-    # nwise_parser.add_argument('-n', action='store', dest='n', default=2, help='n in nwise')
-    # nwise_parser.add_argument('--coverage', action='store', dest='coverage', default=100, help='Requested coverage in percent. The generator will stop after the requested percent of n-tuples will be covered.')
-
-    # pairwise_parser = gen_parsers.add_parser('--pairwise')
-    # pairwise_parser.add_argument('--coverage', action='store', dest='coverage', default=100, help='Requested coverage in percent. The generator will stop after the requested percent of n-tuples will be covered.')
-    
-    # cartesian_parser = gen_parsers.add_parser('--cartesian')
-    # cartesian_parser.add_argument('--coverage', action='store', dest='coverage', default=100, help='Requested coverage in percent. The generator will stop after the requested percent of n-tuples will be covered.')
-
-    # random_parser = gen_parsers.add_parser('--random')
-    # random_parser.add_argument('--length', action='store', dest='length', default=1, help='Number of test cases to generate')
-    # random_parser.add_argument('--duplicates', action='store_true', dest='duplicates', help='If used, the same test can appear more than once in the generated suite')
-    # random_parser.add_argument('--adaptive', action='store_true', dest='adaptive', help='If used, the generator will try to generate tests that are furthest possible from already generated once (in Hamming distance)')
-
-    # static_parser = gen_parsers.add_parser('--static')
-    # static_parser.add_argument('--suites', help='list of test suites that will be fetched from the ecFeed service. If skipped, all test suites will be fetched')
-
     generator_group = required_args.add_mutually_exclusive_group(required=True)
     generator_group.add_argument('--pairwise', dest='data_source', action='store_const', const='pairwise', help='Use pairwise generator. Equal to --nwise -n 2')
     generator_group.add_argument('--nwise', dest='data_source', action='store_const', const=DataSource.NWISE, help='Use NWise generator')
@@ -88,17 +69,6 @@ def parse_arguments():
     other_arguments.add_argument('--output', '-o', dest='output', action='store', help='output file. If omitted, the standard output will be used')
 
     args = vars(parser.parse_args())
-
-    # if ('choices' in args or 'constraints' in args) and ('--static' in args):
-    #     print('--choices and --constraints options not available for --static generator')
-    # if 'n' in args and '--nwise' not in args:
-    #     print('-n option is valid only for --nwise generator')
-    # if 'coverage' in args and 'nwise' not in args and 'pairwise' not in args and 'cartesian' not in args:
-    #     print('--coverage option is valid only for --nwise, --pairwise or --cartesian generator')
-    # if ('length' in args or 'duplicates' in args or 'adaptive' in args) and 'random' not in args:
-    #     print('--length, --duplicates and --adaptive options are valid only for --random, --pairwise or --random generator')
-    # if 'suites' in args and 'suites' not in args:
-    #     print('--length, --duplicates and --adaptive options are valid only for --random, --pairwise or --random generator')
 
     if 'choices' in args and args['choices'] != None: 
         args['choices'] = json.loads(args['choices'].replace('\'', '"'))
