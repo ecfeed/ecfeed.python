26b207b634752b1306f6a467ac1f7213d525cee7
Patryk Chamuczyński <p.chamuczynski@radytek.com>
2020-09-07 14:20:47 +0200

    updated pip package version to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ecfeed", 
-    version="1.0.0b5",
+    version="2.0.0b5",
     author="EcFeed AS",
     author_email="mail@ecfeed.com",
     description="Python interface for accessing EcFeed online generator service",
