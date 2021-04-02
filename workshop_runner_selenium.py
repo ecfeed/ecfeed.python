fecb2c5b8c2b25c55c7c550b6d594554a82f09aa
krzysztof skorupski <k.skorupski@testify.no>
2021-04-02 15:56:39 +0200

    selenium tests

--- workshop_runner_selenium.py
+++ workshop_runner_selenium.py
@@ -28,38 +28,38 @@ driver = webdriver.Firefox()
 
 # ---------------------------------------------------------
 
-def setForm(driver, values):
-    validateInput(form["input"], values)
+def set_form(values):
+    validate_values(form["input"], values)
 
-    setFormText(driver, values[0])
-    setFormSelect(driver, values[1])
+    set_form_text(values[0])
+    set_form_combo(values[1])
 
-def validateInput(reference, input):
+def validate_values(reference, values):
 
-    if len(input) != 2:
+    if len(values) != 2:
         raise Exception("The dimension of the input array is incorrect.")
         
-    if len(reference[0]) != len(input[0]): 
+    if len(reference[0]) != len(values[0]): 
         raise Exception("The number of the input text fields is incorrect.")
         
-    if len(reference[1]) != len(input[1]):
+    if len(reference[1]) != len(values[1]):
         raise Exception("The number of the input select fields is incorrect.")
 
-def setFormText(driver, values):
+def set_form_text(values):
     for i in range(len(form["input"][0])):
         element = driver.find_element_by_id(form["input"][0][i])
         element.clear()
         element.send_keys(values[i])
         
-def setFormSelect(driver, values):
+def set_form_combo(values):
     for i in range(len(form["input"][1])):
         Select(driver.find_element_by_id(form["input"][1][i])).select_by_visible_text(values[i])
 
-def execute(driver):
+def execute():
     for element in form["execute"]:
         driver.find_element_by_id(element).click()
 
-def getResponse(driver):
+def get_response():
     response = []
 
     for i in range(len(form["output"])):
@@ -69,32 +69,42 @@ def getResponse(driver):
     
 # ---------------------------------------------------------
 
+def set_selenium_form(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email, test_id):
+    timeStart = time.time()
+
+    try:
+        set_form([[Name, Address, Quantity, Phone, Email], [Country, Product, Color, Size, Payment, Delivery]])
+        execute()
+    except NoSuchElementException as err:
+        assert False, ecfeed.feedback(test_id, False, comment=err.msg, custom={'Error type': 'Input'})
+    
+    timeEnd = time.time()
+
+    return {
+        "response": get_response(),
+        "time": int(1000 * (timeEnd - timeStart))
+    }
+
+def process_selenium_results(response, test_id):
+    
+    assert response["response"][0] == " Request accepted", ecfeed.feedback(test_id, False, duration=response["time"], comment=''.join(e + ' ' for e in response["response"]), custom={'Error type': 'Output'})
+    
+    ecfeed.feedback(test_id, True, duration=response["time"])
+
+# ---------------------------------------------------------
+
 @pytest.fixture(scope='session', autouse=True)
 def setup():
     driver.get(endpoint) 
     yield driver
     driver.close()
 
-@pytest.mark.parametrize(ecfeed.test_header(method, feedback=True), ecfeed.nwise(method=method, feedback=True, constraints='NONE'))
+@pytest.mark.parametrize(ecfeed.test_header(method, feedback=True), ecfeed.nwise(method=method, feedback=True))
 def test_method_random(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email, test_id):
 
-    timeStart = time.time()
-
-    try:
-        setForm(driver, [[Name, Address, Quantity, Phone, Email], [Country, Product, Color, Size, Payment, Delivery]])
-    except NoSuchElementException as err:
-        assert False, ecfeed.feedback(test_id, False, comment=err.msg, custom={'Error type': 'Input'})
-    
-    execute(driver)
-    response = getResponse(driver)
-    timeEnd = time.time()
-    timeTotal = int(1000 * (timeEnd - timeStart))
-
+    response = set_selenium_form(Country, Name, Address, Product, Color, Size, Quantity, Payment, Delivery, Phone, Email, test_id)
     time.sleep(1)
+    process_selenium_results(response, test_id)
 
-    assert response[0] == " Request accepted", ecfeed.feedback(test_id, False, duration=timeTotal, comment=''.join(e + ' ' for e in response), custom={'Error type': 'Output'})
     
-    ecfeed.feedback(test_id, True, duration=timeTotal)
     
-
-
